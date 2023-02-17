2a86b2be2afe43f036ba672c3832336eb54d0ad2
felipe toledo <felipetoledo07@gmail.com>
2023-02-17 15:17:29 -0300

    matrix calculation and write in the front

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
 let buttonMatrix;
 let monthMatrix;
+let resultMatrix;
 
 function updateData() {
     
     buttonMatrix = [
-        [document.getElementById("Ap"), document.getElementById("Bp"), document.getElementById("Cp")],
-        [document.getElementById("AG"), document.getElementById("BG"), document.getElementById("CG")]
+        [parseInt(document.getElementById("Ap").value), parseInt(document.getElementById("Bp").value), parseInt(document.getElementById("Cp").value)],
+        [parseInt(document.getElementById("AG").value), parseInt(document.getElementById("BG").value), parseInt(document.getElementById("CG").value)]
     ];
     
     
     monthMatrix = [
-        [document.getElementById("MA"), document.getElementById("JA")],
-        [document.getElementById("MB"), document.getElementById("JB")],
-        [document.getElementById("MC"), document.getElementById("JC")]
+        [parseInt(document.getElementById("MA").value), parseInt(document.getElementById("JA").value)],
+        [parseInt(document.getElementById("MB").value), parseInt(document.getElementById("JB").value)],
+        [parseInt(document.getElementById("MC").value), parseInt(document.getElementById("JC").value)]
+    ]
+    resultMatrix = [
+        [0, 0],
+        [0, 0]
     ]
 }
+
+function calculate() {
+
+    for (let i = 0; i < buttonMatrix.length; i++) {
+        for (let j = 0; j < monthMatrix[i].length; j++) {
+            for (let k = 0; k < buttonMatrix[i].length; k++) {
+                resultMatrix[i][j] += buttonMatrix[i][k] * monthMatrix[k][j]
+            }
+        }
+    }
+}
+
+function write() {
+   
+    document.getElementById("Mp").innerHTML=resultMatrix[0][0]
+    document.getElementById("Jp").innerHTML=resultMatrix[0][1]
+    document.getElementById("MG").innerHTML=resultMatrix[1][0]
+    document.getElementById("JG").innerHTML=resultMatrix[1][1]
+}
+
+
+let inputs = document.querySelectorAll("input")
+
+inputs.forEach(function(input) {
+    input.addEventListener('change', event => {
+        updateData()
+        calculate()
+        write()
+    })
+})
+
+updateData()
+calculate()
+write()
