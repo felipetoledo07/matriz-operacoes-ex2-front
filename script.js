7e6ced25f4d96546085e7b7dfc53b55840a7cc1c
felipe toledo <felipetoledo07@gmail.com>
2023-02-17 14:37:51 -0300

    data integration

---
+++ script.js
@@ -0,0 +1,17 @@
+let buttonMatrix;
+let monthMatrix;
+
+function updateData() {
+    
+    buttonMatrix = [
+        [document.getElementById("Ap"), document.getElementById("Bp"), document.getElementById("Cp")],
+        [document.getElementById("AG"), document.getElementById("BG"), document.getElementById("CG")]
+    ];
+    
+    
+    monthMatrix = [
+        [document.getElementById("MA"), document.getElementById("JA")],
+        [document.getElementById("MB"), document.getElementById("JB")],
+        [document.getElementById("MC"), document.getElementById("JC")]
+    ]
+}
